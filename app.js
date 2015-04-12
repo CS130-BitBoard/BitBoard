c94d34b3a8468a2e4fbb829f4bfbfffbeb4fabc5
nitin7 <nitin7@ucla.edu>
2015-04-12 16:09:29 -0700

    Added pencil functionality
    Can draw collaboratively
    Implemented using socket.io

---
+++ app.js
@@ -0,0 +1,47 @@
+
+
+var express = require('express');
+var routes = require('./routes');
+var user = require('./routes/user');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon('./node_modules/express/node_modules/connect/node_modules/static-favicon/favicon.ico'));
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', routes.index);
+
+var server = http.createServer(app).listen(app.get('port'));
+var io= require('socket.io').listen(server, function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+var usernames = {};
+
+io.sockets.on('connection', function(socket) {
+    socket.on( 'startPath', function( data, sessionId ) {
+        socket.broadcast.emit( 'startPath', data, sessionId );
+
+    });
+    socket.on( 'continuePath', function( data, sessionId ) {
+        socket.broadcast.emit( 'continuePath', data, sessionId );
+
+    });
+    socket.on('clearCanvas', function() {
+    	socket.broadcast.emit('clearCanvas');
+    });
+});
+
+
+
+
---
+++ public/javascripts/draw.js
@@ -0,0 +1,84 @@
+var paths = {};
+var sessionId = io.socket.sessionid;
+
+$("#pencil").click(function() {
+    tool1.activate();
+    $("#pencil").addClass('selected');
+});
+
+
+$("#clear").click(function() {
+    clearCanvas();
+    view.draw();
+    $("#pencil").removeClass('selected');
+    io.emit('clearCanvas');
+});
+
+function clearCanvas() {
+    project.activeLayer.removeChildren();
+}
+
+var path;
+var color;
+
+function onMouseDown(event) {
+
+    var point = event.point;
+    var data = {
+        point: point,
+        color: "#0000000"
+    }
+    startPath(data, sessionId);
+    emit("startPath", data, sessionId);
+}
+
+//pencil
+tool1 = new Tool();
+tool1.onMouseDown = onMouseDown;
+
+tool1.onMouseDrag = function(event) {
+    var point = event.point;
+    var data = {
+        point: point,
+        tool: "tool1"
+    };
+    continuePath(data, sessionId);
+    emit("continuePath",data, sessionId);
+}
+
+function startPath(data, sessionId) {
+    paths[sessionId] = new Path();
+    paths[sessionId].strokeColor = data.color;
+    paths[sessionId].add(data.point);
+}
+
+function continuePath(data, sessionId) {
+    var path = paths[sessionId];
+    if (data.tool === "tool1"){
+        path.add(data.point);
+    }
+}
+
+function emit(eventName, data, sessionId) {
+    io.emit(eventName, data, sessionId);
+}
+
+io.on('startPath', function(data, sessionId) {
+    if (data.point){
+        data.point = new Point(data.point[1], data.point[2]);
+    }
+    startPath(data, sessionId);
+});
+
+io.on('continuePath', function(data, sessionId) {
+    if (data.point){
+        data.point = new Point(data.point[1], data.point[2]);
+    }
+   continuePath(data, sessionId);
+    view.draw();
+});
+
+io.on('clearCanvas', function() {
+    clearCanvas();
+    view.draw();
+});
