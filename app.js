9146e5acf2250b90dac93e41798c799336f17864
Ryan Hansberry <rhansby@gmail.com>
2015-05-25 17:00:08 -0700

    Minor cleanup of app.js.

--- app.js
+++ app.js
@@ -26,66 +26,68 @@ app.get('/', indexRoutes.index);
 app.post('/boards', boardRoutes.create);
 app.get('/boards/:boardId', boardRoutes.get);
 
-//Holds the current state of each board.
-var board_state = {};
-//Key:boardID Value: list of users
+// Holds the current state of each board.
+var boardStates = {};
+// Key:boardID Value: list of users
 var users = {};
 
 io.on('connection', function(socket) {
     var boardId = '';
 
-    socket.on('joinBoard', function(boardid, userid) {
-        boardId = boardid;
+    socket.on('joinBoard', function(newBoardId, userId) {
+        boardId = newBoardId;
         socket.join(boardId);
-        console.log('BoardId: ', boardid);
-        if (!users[boardid]) {
-            users[boardid] = [];
+        if (!users[boardId]) {
+            users[boardId] = [];
         }
-        if (userid != null) {
-            socket.userid = userid;
-            users[boardid].push(userid);
+        if (userId != null) {
+            socket.userId = userId;
+            users[boardId].push(userId);
         }
         socket.emit('updateChatbox', '', ' you have joined');
-        socket.broadcast.to(boardId).emit('updateChatbox', '', ' ' + userid + ' has joined');
+        socket.broadcast.to(boardId).emit('updateChatbox', '', ' ' + userId + ' has joined');
 
-        console.log(users);
+        // TODO: remove this
+        console.log('users:', users);
+        console.log('boardStates:', boardStates);
+
         // Replay messages to new clients
-        if (board_state[boardId]) {
-            var messages = board_state[boardId];
+        if (boardStates[boardId]) {
+            var messages = boardStates[boardId];
             for (var i = 0; i < messages.length; i++) {
                 var m = messages[i];
                 socket.emit(m.type, m.data, m.sessionId);
             }
         } else {
             // We are the first in the room, create an empty state
-            board_state[boardId] = [];
+            boardStates[boardId] = [];
         }
     });
 
     socket.on('startPath', function(data, sessionId) {
-        board_state[boardId].push(new Message('startPath', data, sessionId));
+        boardStates[boardId].push(new Message('startPath', data, sessionId));
         socket.broadcast.to(boardId).emit('startPath', data, sessionId);
     });
     socket.on('continuePath', function(data, sessionId) {
-        board_state[boardId].push(new Message('continuePath', data, sessionId));
+        boardStates[boardId].push(new Message('continuePath', data, sessionId));
         socket.broadcast.to(boardId).emit('continuePath', data, sessionId);
     });
     socket.on('clearCanvas', function() {
-        board_state[boardId].length = 0; // Clear the array
+        boardStates[boardId].length = 0; // Clear the array
         socket.broadcast.to(boardId).emit('clearCanvas');
     });
 
     socket.on('sendChatMessage', function(message) {
-        socket.broadcast.to(boardId).emit('updateChatbox', socket.userid, message);
+        socket.broadcast.to(boardId).emit('updateChatbox', socket.userId, message);
     });
 
     socket.on('disconnect', function() {
-        if (socket.userid != null) {
-            delete users[boardId][users[boardId].indexOf(socket.userid)];
+        if (socket.userId != null) {
+            delete users[boardId][users[boardId].indexOf(socket.userId)];
             if (users[boardId].length == 0) {
                 delete users.boardId;
             }
-            socket.broadcast.to(boardId).emit('updateChatbox', '', socket.userid + ' disconnected');
+            socket.broadcast.to(boardId).emit('updateChatbox', '', socket.userId + ' disconnected');
         }
     });
 });
